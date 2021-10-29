40106c19aea40b0522aa163bacff36b276460b83
José Venzke <josevenzkejr@gmail.com>
2021-10-29 12:22:10 -0300

    created add and get user endpoints

--- pomodoro/api/views.py
+++ pomodoro/api/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.contrib.auth.hashers import make_password
+
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
 
 # Create your views here.
+@api_view(['POST'])
+def add_user(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    email = request.POST.get('email')
+
+    if not  all([username,password,email]):
+        raise Exception('erro')
+
+    new_user = User.objects.create_user(username=username,email=email,password=password)
+    
+    serialized_user = UserSerializer(new_user).data
+
+    return Response({'user':serialized_user})
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_user(request):
+    user = request.user
+    serialized_user = UserSerializer(user,many=True).data
+
+    return Response({'user':serialized_user})
+
+
