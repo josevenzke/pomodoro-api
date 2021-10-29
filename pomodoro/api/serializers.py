e5d019847705cfcd5a3e25bf05f54dd4024d143a
José Venzke <josevenzkejr@gmail.com>
2021-10-29 18:47:26 -0300

    new serializers

--- pomodoro/api/serializers.py
+++ pomodoro/api/serializers.py
@@ -1,7 +1,14 @@
+from api.models import PomodoroTree
 from rest_framework import serializers
+from .models import *
 from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username','email','date_joined']
+
+class PomodoroTreeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PomodoroTree
+        fields = ['user','all_time_pomodoros','current_pomodoros','time_for_next_pomodoro']
