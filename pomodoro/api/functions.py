5931727153912c7e5442a7f5d53ffffa83dff848
José Venzke <josevenzkejr@gmail.com>
2021-10-30 01:09:50 -0300

    created new function almost equal to the other one thats bad

--- pomodoro/api/functions.py
+++ pomodoro/api/functions.py
@@ -1,12 +1,11 @@
-from.models import Pomodoro
+from.models import Pomodoro, TimeSpent
 import pandas
 from datetime import timedelta
 
-def get_pomodoros_specific_period(date_start,date_end) -> dict:
-    pomodoros = Pomodoro.objects.filter(created_at__range=(date_start,date_end)).values()
+def get_pomodoros_specific_period(date_start,date_end,pomotree) -> dict:
+    pomodoros = Pomodoro.objects.filter(created_at__range=(date_start,date_end),pomodorotree_id=pomotree.id).values()
     all_dates = pandas.date_range(date_start+timedelta(days=1),date_end,freq='d')
     
-
     pomodoro_per_day = {}
     for date in all_dates:
         pomodoro_per_day[date.strftime("%Y-%m-%d")] = 0
@@ -17,3 +16,18 @@ def get_pomodoros_specific_period(date_start,date_end) -> dict:
             pomodoro_per_day[day]+=1
     
     return pomodoro_per_day
+
+def get_time_spent_period(date_start,date_end,pomotree):
+    time_entries = TimeSpent.objects.filter(date__range=(date_start,date_end),pomodorotree_id=pomotree.id).values()
+    all_dates = pandas.date_range(date_start+timedelta(days=1),date_end,freq='d')
+    
+    time_per_day = {}
+    for date in all_dates:
+        time_per_day[date.strftime("%Y-%m-%d")] = 0
+    
+    for time in time_entries:
+        day = time['date'].strftime("%Y-%m-%d") 
+        if day in time_per_day:
+            time_per_day[day] += int(time['time_spent'])
+
+    return time_per_day
