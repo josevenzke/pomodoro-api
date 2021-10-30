acbb1ecb1d2fcc0d467ade09d0875805099dcab3
José Venzke <josevenzkejr@gmail.com>
2021-10-30 03:13:57 -0300

    new url names

--- pomodoro/api/urls.py
+++ pomodoro/api/urls.py
@@ -9,12 +9,12 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('get-user/', views.get_user),
-    path('add-user/',views.add_user),
-    path('add-time/', views.add_time),
-    path('get-pomodoros/', views.get_pomodoros),
-    path('get-timespent/', views.get_time_spent),
-    path('add-reward/', views.add_reward),
-    path('get-rewards/', views.get_rewards),
-    path('buy-reward/<int:id_>/', views.buy_reward)
+    path('get-userinfo/', views.get_user, name='user-info'),
+    path('create-user/',views.add_user,name='create-user'),
+    path('add-time/', views.add_time, name='add-time'),
+    path('get-pomodoros-data/', views.get_pomodoros, name='get-pomodoro-data'),
+    path('get-timespent-data/', views.get_time_spent,name='get-timespent-data'),
+    path('add-reward/', views.add_reward, name='add-reward'),
+    path('get-rewards/', views.get_rewards, name='get-rewards'),
+    path('buy-reward/<int:id_>/', views.buy_reward, name='buy-reward')
 ]
