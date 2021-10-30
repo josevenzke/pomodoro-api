7bfc2592c255c287c3dac6e1c45d489a9d922c7e
José Venzke <josevenzkejr@gmail.com>
2021-10-30 03:21:42 -0300

    now using env variables

--- pomodoro/pomodoro/settings.py
+++ pomodoro/pomodoro/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(8_e-kkqrl#bk0hd=uy(0h7o#0l^i*uqx#io+5j%cq(=0!sj6$'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG",default=False)
 
 ALLOWED_HOSTS = []
 
