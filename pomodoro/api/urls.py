c48db531eaa53c035deb747aa7e083826c762e35
Jose <josevenzkejr@gmail.com>
2021-11-09 21:13:26 -0300

    removed simplejwt urls

--- pomodoro/api/urls.py
+++ pomodoro/api/urls.py
@@ -1,14 +1,8 @@
 from django.urls import path
 from api import views
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 urlpatterns = [
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('get-userinfo/', views.get_user, name='user-info'),
     path('create-user/',views.add_user,name='create-user'),
     path('add-time/', views.add_time, name='add-time'),
